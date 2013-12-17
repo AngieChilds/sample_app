eb49b2a6c264f31f6bdeeb1c614a1006edf60eaf
AngieChilds <pirateprincess_avh@yahoo.com>
2013-12-17 02:12:45 +0200

    sign up sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
   describe "with a password that's too short" do
@@ -34,5 +35,6 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
-  end
+    
+end
 end
